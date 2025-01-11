2acfc51c41415f6bdf7d8d8b7f1dd5924654d96c
gertrudemuthoni <git24003@byui.edu>
2025-01-11 21:50:08 +0300

    core requirements erercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
-}
+        Console.WriteLine("Welcome!");
+        Console.WriteLine("Enter your grade percentage?: ");
+        /*Also written as
+        string grade = console.ReadLine();
+        int percentage = int.Parse(grade);
+        */
+        int percent = int.Parse(Console.ReadLine());
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (percent >= 70)
+        {
+            Console. WriteLine("You passed this class!");
+        }
+
+        else
+        {
+            Console.WriteLine("Try again next semester!");
+        }
+    }    
+}        
+    
