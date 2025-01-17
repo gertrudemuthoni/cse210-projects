f0d46846157bf00e9cf4d5cff0f356f7643286f7
gertrudemuthoni <git24003@byui.edu>
2025-01-18 00:52:16 +0300

    Resumes updated

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,14 @@
 using System;
 
-class Program
+public class Job
 {
-    static void Main(string[] args)
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
 }
