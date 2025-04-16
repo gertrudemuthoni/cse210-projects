51141516a285bf7ceb28824bb88303d00f365d5d
gertrudemuthoni <git24003@byui.edu>
2025-04-16 18:36:06 +0300

    goals program

---
+++ week06/EternalQuest/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // initialize goal manager
+        GoalManager goalManager = new GoalManager();
+
+        // start the program
+        goalManager.Start();
+    }
+}
